5a4b00d635b95bb1acb1b702f4c716ce429fa96b
agnessuarna <agnessuarna@mail.ugm.ac.id>
2022-11-20 16:39:45 +0700

    add form login

--- 3R/Program.cs
+++ 3R/Program.cs
@@ -1,19 +1,24 @@
+using WindowsFormsApp1;
 using WindowsFormsApp1.Forms;
 
 namespace _3R
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Home());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Login());
+            //Application.Run(new View_Cars());
+            //Application.Run(new Add_Cars());
+            //Application.Run(new Add_Members()); 
+            //Application.Run(new View_Members());
+            //Application.Run(new Rent_Car());
+            //Application.Run(new Return_Car());
+            //Application.Run(new Garage());
+            //Application.Run(new Home());
         }
     }
 }
